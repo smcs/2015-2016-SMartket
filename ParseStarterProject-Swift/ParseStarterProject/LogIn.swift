e798482ad1318d10a57fb83549660cdce44db7f8
Seung Hwan Baek <seunghwanbaek@stmarksschool.org>
2016-02-19 09:40:32 -0500

    Initial Commit!

---
+++ ParseStarterProject-Swift/ParseStarterProject/LogIn.swift
@@ -0,0 +1,26 @@
+//
+//  LogIn.swift
+//  ParseStarterProject-Swift
+//
+//  Created by David Baek on 2/19/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogIn : UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        
+    }
+    
+}
