9d4a0d5fc39205444ed375c9002f9aa1b45326ec
Seung Hwan Baek <seunghwanbaek@stmarksschool.org>
2016-03-09 13:50:31 -0500

    UI added missing constraints

--- SMartket-master/ParseStarterProject-Swift/ParseStarterProject/SellViewController.swift
+++ SMartket-master/ParseStarterProject-Swift/ParseStarterProject/SellViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class SellViewController: UIViewController {
 
+    @IBOutlet var name: UITextField!
+    @IBOutlet var isbn: UITextField!
+    @IBOutlet var condition: UITextField!
+    @IBOutlet var amount: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        name.resignFirstResponder()
+        isbn.resignFirstResponder()
+        condition.resignFirstResponder()
+        amount.resignFirstResponder()
+        
+        return true
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
